60411f1120cdd8c9b201704e45430236042bf20d
Jonathan <jtan13122004@gmail.com>
2025-03-12 00:01:07 +0800

    Finished initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="m-auto flex w-xs justify-center py-8">
+      <h1 className="text-center text-3xl text-gray-700">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
